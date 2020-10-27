984c3c813489a11b0aa281b3b65a31dd7d816f7c
Nathan Bullock <nathanbullock@gmail.com>
2020-10-27 15:42:06 -0700

    Pull Atom feed generation out of main

--- cmd/ssg/main.go
+++ cmd/ssg/main.go
@@ -19,58 +19,6 @@ var (
 	cfg config.Config
 )
 
-// feed TODO
-type feed struct {
-	SiteURL   string
-	SiteTitle string
-	SiteID    string
-	Author    string
-}
-
-func createAtomFeed(feed feed, configs []post.Entry) ([]byte, error) {
-	ents := []post.Entry{}
-	for i := range configs {
-		if configs[i].Date != "" {
-			ents = append(ents, configs[i])
-		}
-	}
-	if len(ents) == 0 {
-		return nil, fmt.Errorf("Can't create XML feed, no entries")
-	}
-	sort.Sort(post.ByDate(ents))
-
-	s := ""
-	s += fmt.Sprintf("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n")
-	s += fmt.Sprintf("<feed xmlns=\"http://www.w3.org/2005/Atom\">\n")
-	s += fmt.Sprintf("  <title>%s</title>\n", feed.SiteTitle)
-	s += fmt.Sprintf("  <link href=\"%s/\" />\n", feed.SiteURL)
-	s += fmt.Sprintf("  <updated>%s</updated>\n", ents[0].Date)
-	s += fmt.Sprintf("  <id>%s</id>\n", feed.SiteID)
-
-	for _, e := range ents {
-		if e.Date != "" {
-			s += fmt.Sprintf("<entry>\n")
-			s += fmt.Sprintf("  <title>%s</title>\n", e.Title)
-			s += fmt.Sprintf("  <link href=\"%s%s\" />\n", feed.SiteURL, e.SitePath)
-			s += fmt.Sprintf("  <updated>%s</updated>\n", e.Date)
-			s += fmt.Sprintf("  <id>%s%s</id>\n", feed.SiteURL, e.SitePath)
-			s += fmt.Sprintf("  <author><name>%s</name></author>\n", feed.Author)
-			s += fmt.Sprintf("  <content type=\"html\"><![CDATA[\n")
-			s += fmt.Sprintf("%s\n", e.Content)
-			s += fmt.Sprintf("  ]]></content>\n")
-			s += fmt.Sprintf("</entry>\n")
-		}
-	}
-	s += fmt.Sprintf("</feed>\n")
-
-	body := []byte(s)
-	body = bytes.ReplaceAll(body, []byte("/img/"), []byte(cfg.ImageURL+"/"))
-	body = bytes.ReplaceAll(body, []byte("/pdf/"), []byte(cfg.ImageURL+"/"))
-	body = bytes.ReplaceAll(body, []byte("href=\"/"), []byte("href=\""+cfg.URL+"/"))
-
-	return body, nil
-}
-
 func postIndexEntry(e post.Entry) string {
 	var cnt string
 	img := e.Image
@@ -264,13 +212,14 @@ func walk() error {
 				return fmt.Errorf("parsing %s: %w", ent.FilePath, err)
 			}
 		} else if ent.Type == "atom" {
-			var feed feed
+			var feed util.Feed
 			feed.SiteTitle = cfg.Title
 			feed.SiteURL = cfg.URL
 			feed.SiteID = cfg.URL + "/"
 			feed.Author = cfg.Author
+			feed.ImageURL = cfg.ImageURL
 
-			body, err := createAtomFeed(feed, configs)
+			body, err := util.CreateAtomFeed(feed, configs)
 			if err != nil {
 				return err
 			}

337f5dd35ba4c17274757958056df225fd7b80db
Nathan Bullock <nathanbullock@gmail.com>
2020-11-13 05:44:02 -0800

    Remove a number of no longer needed post and site fields

--- internal/config/post.go
+++ internal/config/post.go
@@ -11,16 +11,10 @@ import (
 
 // Post is the structure used for a given web post entry.
 type Post struct {
-	Template string `yaml:"template"`
-	Title    string `yaml:"title"`
-	Date     string `yaml:"date"`
-	Author   string `yaml:"author"`
-
-	RelatedPosts []string               `yaml:"related"`
-	Snippet      string                 `yaml:"snippet"`
-	Image        string                 `yaml:"image"`
-	Type         string                 `yaml:"type"`
-	Custom       map[string]interface{} `yaml:"custom"`
+	Template string                 `yaml:"template"`
+	Title    string                 `yaml:"title"`
+	Date     string                 `yaml:"date"`
+	Custom   map[string]interface{} `yaml:"custom"`
 
 	FilePath string
 	SitePath string
